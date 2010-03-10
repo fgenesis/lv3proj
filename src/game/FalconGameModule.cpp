7d73167a6041b0617e177d234276ef80b5c49578
fgenesis <false.genesis@googlemail.com>
2010-03-10 22:59:22 +0100

    added include_ex() function to FalconGameModule, that does not create .fam files

--- src/game/FalconGameModule.cpp
+++ src/game/FalconGameModule.cpp
@@ -113,7 +113,7 @@ Falcon::CoreObject* fal_ObjectCarrier::factory( const Falcon::CoreClass *cls, vo
     if(!obj)
     {
         Falcon::AutoCString cclsname(classname);
-        logerror("ObjectCarrier factory: Unable to instanciate an object of class '%s'", cclsname.c_str());
+        logerror("ObjectCarrier factory: Unable to instantiate an object of class '%s'", cclsname.c_str());
         throw new Falcon::AccessError( Falcon::ErrorParam( Falcon::e_noninst_cls ) );
     }
 
@@ -685,11 +685,103 @@ void forbidden_init(Falcon::VMachine *vm)
     throw new Falcon::AccessError( Falcon::ErrorParam( Falcon::e_noninst_cls ) );
 }
 
+FALCON_FUNC fal_include_ex( Falcon::VMachine *vm )
+{
+    Falcon::Item *i_file = vm->param(0);
+    Falcon::Item *i_enc = vm->param(1);
+    Falcon::Item *i_path = vm->param(2);
+    Falcon::Item *i_syms = vm->param(3);
+
+    if( i_file == 0 || ! i_file->isString()
+        || (i_syms != 0 && ! (i_syms->isDict() || i_syms->isNil())  )
+        || (i_enc != 0 && !(i_enc->isString() || i_enc->isNil()) )
+        || (i_path != 0 && !(i_path->isString() || i_path->isNil()) )
+        )
+    {
+        throw new Falcon::ParamError(
+            Falcon::ErrorParam( Falcon::e_inv_params, __LINE__ )
+            .origin(Falcon::e_orig_runtime)
+            .extra( "S,[S],[S],[D]" ) );
+    }
+
+    // create the loader/runtime pair.
+    Falcon::ModuleLoader cpl( i_path == 0 || i_path->isNil() ? vm->appSearchPath() : Falcon::String(*i_path->asString()) );
+    cpl.delayRaise(true);
+    cpl.compileTemplate(false);
+    cpl.compileInMemory(true);
+    cpl.alwaysRecomp(true);
+    cpl.saveModules(false);
+    Falcon::Runtime rt( &cpl, vm );
+    rt.hasMainModule( false );
+
+    // minimal config
+    if ( i_enc != 0 && ! i_enc->isNil() )
+    {
+        cpl.sourceEncoding( *i_enc->asString() );
+    }
+
+    bool execAtLink = vm->launchAtLink();
+
+    //! Copy the filename so to be sure to display it correctly in an eventual error.
+    Falcon::String fileName = *i_file->asString();
+    fileName.bufferize();
+
+    // load and link
+    try
+    {
+        rt.loadFile( fileName, false );
+        vm->launchAtLink( i_syms == 0 || i_syms->isNil() );
+        Falcon::LiveModule *lmod = vm->link( &rt );
+
+        // shall we read the symbols?
+        if( lmod != 0 && ( i_syms != 0 && i_syms->isDict() ) )
+        {
+            Falcon::CoreDict *dict = i_syms->asDict();
+
+            // traverse the dictionary
+            Falcon::Iterator iter( &dict->items() );
+            while( iter.hasCurrent() )
+            {
+                // if the key is a string and a corresponding item is found...
+                Falcon::Item *ival;
+                if ( iter.getCurrentKey().isString() &&
+                    ( ival = lmod->findModuleItem( *iter.getCurrentKey().asString() ) ) != 0 )
+                {
+                    // copy it locally
+                    iter.getCurrent() = *ival;
+                }
+                else {
+                    iter.getCurrent().setNil();
+                }
+
+                iter.next();
+            }
+        }
+
+        // reset launch status
+        vm->launchAtLink( execAtLink );
+    }
+    catch(Falcon::Error* err)
+    {
+        Falcon::CodeError *ce = new Falcon::CodeError( Falcon::ErrorParam( Falcon::e_loaderror, __LINE__ ).
+            extra( fileName ) );
+
+        ce->appendSubError(err);
+        err->decref();
+
+        // reset launch status
+        vm->launchAtLink( execAtLink );
+        throw ce;
+    }
+}
+
 
 Falcon::Module *FalconGameModule_create(void)
 {
     Falcon::Module *m = new Falcon::Module;
     m->name("GameModule");
+
+    m->addExtFunc("include_ex", fal_include_ex);
 
     Falcon::Symbol *symScreen = m->addSingleton("Screen");
     Falcon::Symbol *clsScreen = symScreen->getInstance();
