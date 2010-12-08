2b5200aa8b752a113c4a06f214b1677f38d2a909
fgenesis <false.genesis@googlemail.com>
2010-12-08 22:12:30 +0100

    forgot something

--- src/shared/FalconObjectModule.cpp
+++ src/shared/FalconObjectModule.cpp
@@ -402,7 +402,7 @@ FALCON_FUNC fal_BaseObject_Remove(Falcon::VMachine *vm)
     fal_ObjectCarrier *self = Falcon::dyncast<fal_ObjectCarrier*>( vm->self().asObject() );
     BaseObject *obj = self->GetObj();
     if(obj) // it can happen that the object is already deleted here, and the ptr is NULL
-        obj->MustDie(true);
+        obj->SetDelete();
 }
 
 FALCON_FUNC fal_ActiveRect_SetBBox(Falcon::VMachine *vm)
