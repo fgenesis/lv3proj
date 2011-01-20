6095bf799632a5c4e4bed1cf6c7e0e692a87865c
fgenesis <false.genesis@googlemail.com>
2011-01-20 12:31:19 +0100

    attempt to fix crash on 64 bit

--- src/shared/PropParser.cpp
+++ src/shared/PropParser.cpp
@@ -9,25 +9,27 @@ void ParsePropData(char *strbuf, char *dn)
     uint32 cpos; // comment?
     std::string file_name;
     StrSplit(strbuf, "\n\x0a\x0d", lines);
-    for(std::vector<std::string>::iterator lin = lines.begin(); lin != lines.end(); lin++)
+    for(std::vector<std::string>::iterator it = lines.begin(); it != lines.end(); it++)
     {
-        if(lin->length() < 3) // must have at least 3 chars per line: 'a=b' is valid, 'a=' is not
+        std::string lin = *it;
+
+        if(lin.length() < 3) // must have at least 3 chars per line: 'a=b' is valid, 'a=' is not
             continue;
 
         // strip comments if there are any
-        if( (cpos = lin->find('#')) != std::string::npos)
+        if( (cpos = lin.find('#')) != std::string::npos)
         {
-            (*lin)[cpos] = '\0';
+            lin[cpos] = '\0';
         }
 
         // strip whitespace at end of line
-        while(lin->find_last_of(" \t") == lin->length()) // length() will never return npos so this check is fine
-            (*lin)[lin->length()] = '\0';
+        while(lin.find_last_of(" \t") == lin.length()) // length() will never return npos so this check is fine
+            lin[lin.length()] = '\0';
 
         // check if its the file name line - [string]
-        if((*lin)[0] == '[')
+        if(lin[0] == '[')
         {
-            file_name = lin->c_str() + 1; // skip '['
+            file_name = lin.c_str() + 1; // skip '['
             uint32 alen = file_name.length();
             if(file_name[alen - 1] == ']') // remove trailing ']'
                 file_name.erase(alen - 1);
@@ -35,12 +37,12 @@ void ParsePropData(char *strbuf, char *dn)
             continue;
         }
 
-        uint32 eqpos = lin->find('=');
-        if(eqpos != std::string::npos && eqpos + 1 < lin->length())
+        uint32 eqpos = lin.find('=');
+        if(eqpos != std::string::npos && eqpos + 1 < lin.length())
         {
-            std::string key = lin->substr(0, eqpos);
-            resMgr.SetPropForFile((char*)file_name.c_str(), (char*)key.c_str(), (char*)lin->c_str() + eqpos + 1);
-            logdebug("Prop '%s=%s' for '%s'", key.c_str(), (char*)lin->c_str() + eqpos + 1, file_name.c_str());
+            std::string key = lin.substr(0, eqpos);
+            resMgr.SetPropForFile((char*)file_name.c_str(), (char*)key.c_str(), (char*)lin.c_str() + eqpos + 1);
+            logdebug("Prop '%s=%s' for '%s'", key.c_str(), (char*)lin.c_str() + eqpos + 1, file_name.c_str());
         }
     }
 }
