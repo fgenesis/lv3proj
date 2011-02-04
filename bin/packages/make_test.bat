ef409785b117c3f6e54f91e1250c6fce0964e4eb
fgenesis <false.genesis@googlemail.com>
2011-02-05 00:43:29 +0100

    forgot to add these files long ago (not important ones)

---
+++ bin/packages/make_test.bat
@@ -0,0 +1,9 @@
+
+..\lvpak.exe -c9 -f screen_test.listfile a screen_test.lvpa
+..\lvpak.exe -c9 -f forbidden_test.listfile a forbidden_test.lvpa
+..\lvpak.exe -c9 -f missing_test.listfile a missing_test.lvpa
+..\lvpak.exe -c9 -f error_test.listfile a error_test.lvpa
+..\lvpak.exe -c9 -f savedata_test.listfile a savedata_test.lvpa
+..\lvpak.exe -c9 -f gfx_test.listfile a gfx_test.lvpa
+
+pause
