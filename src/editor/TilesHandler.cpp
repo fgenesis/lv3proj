fgenesis/lv3proj

commit 8d4bf8a387d4c1daee9c95463c6a9c92b93b8f82

fixed possible editor crash + minor script fix

src/shared/ResourceMgr.cpp
@@ -255,7 +255,7 @@ Anim *ResourceMgr::LoadAnim(char *name)
         memblock *mb = resMgr.LoadTextFile((char*)fn.c_str());
         if(!mb)
         {
-            logerror("LoadAnimFile: Failed to open '%s'", fn.c_str());
+            logerror("LoadAnim: Failed to open '%s'", fn.c_str());
             return NULL;
         }
 
@@ -264,7 +264,7 @@ Anim *ResourceMgr::LoadAnim(char *name)
 
         if(!ani)
         {
-            logerror("LoadAnimFile: Failed to parse '%s'", fn.c_str());
+            logerror("LoadAnim: Failed to parse '%s'", fn.c_str());
             return NULL;
         }
 
@@ -275,6 +275,12 @@ Anim *ResourceMgr::LoadAnim(char *name)
             {
                 loadpath = AddPathIfNecessary(af->filename,relpath);
                 af->surface = resMgr.LoadImg((char*)loadpath.c_str()); // get all images referenced
+                if(!af->surface)
+                {
+                    delete ani; // its not yet registered, must simply delete
+                    logerror("LoadAnim: '%s': Failed to open referenced image '%s'", fn.c_str(), loadpath.c_str());
+                    return NULL;
+                }
                 af->callback.ptr(af->surface); // register callback for auto-deletion
             }
 

bin/scripts/system/DefaultScreen.fal
@@ -5,6 +5,7 @@ function InitDefaultScreen()
     > "Default screen layout created"
 end
 
-InitDefaultScreen()
+// this is broadcasted by init.fal when script initialization is complete
+subscribe("INIT_DONE", InitDefaultScreen)
 
 //export

bin/scripts/system/util.fal
@@ -105,6 +105,7 @@ end
 // <baseClass>:  the class ctor of the base class
 // <params>:     an array with params to pass (e.g. argv()) - NON-METHODIC!
 function super(caller, baseClass, params)
+    if not (params and len(params)): return // in this case, nothing to do, subclass already initialized with no params
     caller.apply(([baseClass] + params)().retrieve()) // some weird, dark magic happens here...
 end
 

commit a38a99b6a34bfb16d37b72041079c90e297fe02d

fixed the problems caused by 8d4bf8a387

src/editor/TilesHandler.cpp
@@ -67,15 +67,17 @@ void DirLoadHelper(std::string dirname, TileLayer *layer, VFSDir *dir, uint32 ma
         BasicTile *tile = AnimatedTile::New(fn.c_str());
         if(tile)
         {
-            SDL_Surface *sf = tile->GetSurface();
-            if(sf->w == 16 && sf->h == 16) // TODO: make less hacky
+            if(SDL_Surface *sf = tile->GetSurface())
             {
-                logdebug("Using '%s' as tile", fn.c_str());
-                layer->SetTile(pos % maxwidth, pos / maxwidth, tile);
-                ++pos;
+                if(sf->w == 16 && sf->h == 16) // TODO: make less hacky
+                {
+                    logdebug("Using '%s' as tile", fn.c_str());
+                    layer->SetTile(pos % maxwidth, pos / maxwidth, tile);
+                    ++pos;
+                }
+                else
+                    logdebug("Skipping '%s' as tile, wrong size", fn.c_str());
             }
-            else
-                logdebug("Skipping '%s' as tile, wrong size", fn.c_str());
 
             tile->ref--;
         }

src/shared/ResourceMgr.cpp
@@ -275,13 +275,13 @@ Anim *ResourceMgr::LoadAnim(char *name)
             {
                 loadpath = AddPathIfNecessary(af->filename,relpath);
                 af->surface = resMgr.LoadImg((char*)loadpath.c_str()); // get all images referenced
-                if(!af->surface)
+                if(af->surface)
+                    af->callback.ptr(af->surface); // register callback for auto-deletion
+                else
                 {
-                    delete ani; // its not yet registered, must simply delete
                     logerror("LoadAnim: '%s': Failed to open referenced image '%s'", fn.c_str(), loadpath.c_str());
-                    return NULL;
+                    // we keep the NULL-ptr anyways
                 }
-                af->callback.ptr(af->surface); // register callback for auto-deletion
             }
 
          logdebug("LoadAnim: '%s' [%s]" , fn.c_str(), vfs.GetFile(fn.c_str())->getSource()); // the file must exist